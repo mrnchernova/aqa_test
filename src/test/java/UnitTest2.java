ece614352f0f5022262b5e21b4b569275fb1542d
mchenova <mchernova3706@gmail.com>
2025-01-26 12:31:48 +0300

    Initial commit

---
+++ src/test/java/UnitTest2.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UnitTest2 {
+    @Test
+    void simpleUnitTest2(){
+        Assertions.assertEquals(1,1);
+    }
+}
